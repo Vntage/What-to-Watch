430da98e635ddc86ac5b65b8cedc3efe96486015
madisonjones3463 <78003853+madisonjones3463@users.noreply.github.com>
2024-12-09 15:43:43 -0500

    Update index.js

--- what-to-watch/src/index.js
+++ what-to-watch/src/index.js
@@ -1,8 +1,15 @@
+import App from './App';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import * as streamingAvailability from "streaming-availability";
 import reportWebVitals from './reportWebVitals';
+
+const RAPID_API_KEY = "0a1ff5a105mshb59e70356a0218ep1017a2jsnee7345c90489";
+
+export const Client = new streamingAvailability.Client(new streamingAvailability.Configuration({
+    apiKey: RAPID_API_KEY
+}));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,4 +21,22 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals(); 
+
+
+
+export let searchResult = await Client.showsApi.searchShowsByFilters(({
+  country: "us"
+}));
+
+export const country = await Client.countriesApi.getCountries({});
+
+searchResult.shows.forEach((show) => {
+  console.log(show.title);
+  console.log(show.overview);
+  show.streamingOptions["us"].forEach((streamingOption) => {
+      if(streamingOption.service.id === "netflix") {
+          console.log(streamingOption.link);
+      }
+  });
+});
