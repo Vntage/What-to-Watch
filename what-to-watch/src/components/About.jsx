9419ec8eb1321b3f864c2716382a51cb461fa5ea
Vntage <142827929+Vntage@users.noreply.github.com>
2024-12-09 19:19:35 -0500

    QoL Changes

--- what-to-watch/src/components/About.jsx
+++ what-to-watch/src/components/About.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
 const About = () => {
-  return <div>
+  return <div style={{
+    display: 'flex',
+    justifyContent: 'center',
+    width: '100%',
+    textAlign: 'center'}}>
     Welcome to our Movie Recommendation Site, developed by Team 8 as part of the CSI418 Software Engineering course. 
     <br/>
-    This platform is designed to help movie enthusiasts discover new films based on their personal tastes and ratings.
+    This platform is designed to help movie enthusiasts discover new films based on their personal tastes and ratings.<br/><br/>
+--------------------------------------------------------------------------------------------------------------------------- <br/>
+    Key features of our site include: <br/><br/>
+
+    User Accounts: Sign up, log in, and manage your profile to track your movie preferences. (Not Complete) <br/>
+    Movie Ratings: Rate the movies you've watched, and influence the recommendations you receive. <br/>
+    Personalized Recommendations: Get tailored movie suggestions through algorithms that match your ratings and preferences. <br/>
+    Search & Filtering: Easily search and filter movies by genre, release year, and more to find exactly what you're looking for. <br/>
+    --------------------------------------------------------------------------------------------------------------------------- <br/>
+Our goal is to provide an intuitive, engaging, and responsive platform that makes movie discovery fun and personalized for all users.
   </div>;
 };
 
