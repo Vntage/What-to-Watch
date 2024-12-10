799f94710f37b3b59509a49816bfe8f16a3a2908
Vntage <142827929+Vntage@users.noreply.github.com>
2024-12-09 19:04:40 -0500

    Quality of Life Changes

--- what-to-watch/src/components/About.jsx
+++ what-to-watch/src/components/About.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const About = () => {
-  return <div>This website was made by Team 8!</div>;
+  return <div>
+    Welcome to our Movie Recommendation Site, developed by Team 8 as part of the CSI418 Software Engineering course. 
+    <br/>
+    This platform is designed to help movie enthusiasts discover new films based on their personal tastes and ratings.
+  </div>;
 };
 
 export default About;
--- what-to-watch/src/components/Home.jsx
+++ what-to-watch/src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Home = () => {
-  return <div>Welcome to the Home page!</div>;
+  return <div>Welcome to What To Watch!</div>;
 };
 
 export default Home;
